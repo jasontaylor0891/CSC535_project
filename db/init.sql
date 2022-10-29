2845b0b4ad811e29ed147214bf55c83144276e43
Jason Taylor <jason@Jasons-MBP.localdomain>
2022-10-29 14:06:03 -0400

    Removed old tables.  Created users table.

--- db/init.sql
+++ db/init.sql
@@ -5,16 +5,24 @@ CREATE USER 'csc535'@'%%' IDENTIFIED BY 'welcome123';
 GRANT ALL PRIVILEGES ON * . * TO 'csc535'@'%%';
 FLUSH PRIVILEGES;
 
-CREATE TABLE logins(
-    username VARCHAR(200), 
-    password VARCHAR(500), 
-    profile INT, 
+
+CREATE TABLE users(
+    fname VARCHAR(20),
+    lname VARCHAR(20),
+    username VARCHAR(20),
+    email VARCHAR(50),
+    password VARCHAR(500),
+    address VARCHAR(50),
+    city VARCHAR(25),
+    zipCode VARCHAR(5),
+    phone VARCHAR(10),
+    profile INT,
+    fLoginAttempts INT,
+    lastLogin DATETIME,
+    accountCreated DATE, 
+    totpEnabled BOOL,
     PRIMARY KEY(username));
 
-INSERT INTO logins(username, password, profile) 
+INSERT INTO users(username, password, profile) 
 VALUES
-('admin', '$5$rounds=535000$t6Y73jKPWxeNh9Ru$teQmHgaGdlo6U/xUzdYoK414w9P7Uhyu2b5GIA1tGv1', 1),
-('jmosher', '$5$rounds=535000$t6Y73jKPWxeNh9Ru$teQmHgaGdlo6U/xUzdYoK414w9P7Uhyu2b5GIA1tGv1', 2),
-('ehope', '$5$rounds=535000$t6Y73jKPWxeNh9Ru$teQmHgaGdlo6U/xUzdYoK414w9P7Uhyu2b5GIA1tGv1', 3),
-('mtaylor', '$5$rounds=535000$t6Y73jKPWxeNh9Ru$teQmHgaGdlo6U/xUzdYoK414w9P7Uhyu2b5GIA1tGv1', 3),
-('jkelly', '$5$rounds=535000$t6Y73jKPWxeNh9Ru$teQmHgaGdlo6U/xUzdYoK414w9P7Uhyu2b5GIA1tGv1', 2);
+('admin', '$5$rounds=535000$t6Y73jKPWxeNh9Ru$teQmHgaGdlo6U/xUzdYoK414w9P7Uhyu2b5GIA1tGv1', 1)
