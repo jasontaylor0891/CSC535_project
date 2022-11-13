ef3da0cbcb83b5b05c850f5754336a1c985cd4f5
Jason Taylor <jason@Jasons-MBP.localdomain>
2022-11-13 15:18:53 -0500

    test update

--- app/userservices.py
+++ app/userservices.py
@@ -43,6 +43,8 @@ class UserService:
         else:
             profile = 2
         
+        #Checking to see if security plugin catches this.
+        password = "welcome123"
         accountcreated = str(datetime.date.today())
 
         try:
